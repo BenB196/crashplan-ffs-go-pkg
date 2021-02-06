66d4e636148a8759bc5351b36e2db13c3c512fd7
BenB196 <8277432+BenB196@users.noreply.github.com>
2021-02-06 13:33:20 -0500

    Made changes to how functions are executed

--- ffsQuery.go
+++ ffsQuery.go
@@ -1,12 +1,5 @@
 package ffs
 
-import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"net/http"
-)
-
 //Structs for FFS Queries
 type Query struct {
 	Groups      []Group `json:"groups"`
@@ -33,47 +26,4 @@ type QueryProblem struct {
 	BadFilter   SearchFilter `json:"badFilter,omitempty"`
 	Description string       `json:"description,omitempty"`
 	Type        string       `json:"type,omitempty"`
-}
-
-func ExecQuery(authData AuthData, ffsURI string, query Query) (*http.Response, error) {
-	//Validate jsonQuery is valid JSON
-	ffsQuery, err := json.Marshal(query)
-	if err != nil {
-		return nil, errors.New("jsonQuery is not in a valid json format")
-	}
-
-	//Make sure authData token is not ""
-	if authData.Data.V3UserToken == "" {
-		return nil, errors.New("authData cannot be nil")
-	}
-
-	//Query ffsURI with authData API token and jsonQuery body
-	req, err := http.NewRequest("POST", ffsURI, bytes.NewReader(ffsQuery))
-
-	//Handle request errors
-	if err != nil {
-		return nil, err
-	}
-
-	//Set request headers
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "v3_user_token "+authData.Data.V3UserToken)
-
-	//Get Response
-	resp, err := http.DefaultClient.Do(req)
-
-	//Handle response errors
-	if err != nil {
-		return nil, err
-	}
-
-	//defer body close
-	defer resp.Body.Close()
-
-	//Make sure http status code is 200
-	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New("Error with gathering file events POST: " + resp.Status)
-	}
-
-	return resp, nil
 }
